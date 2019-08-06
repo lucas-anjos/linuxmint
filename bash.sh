a93a981a8938dcd6159a03d2e42aa71dd7d700a0
lucas-anjos <53832530+lucas-anjos@users.noreply.github.com>
2019-08-06 15:20:52 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@ Lucas dos Anjos Moreira, senac tatuape, aprendendo git e github...
 ctrl + alt + t = terminal no ambiente grafico
 ctrl + alt + f1 = terminal sem ambiente grafico
 ls = listar arquivos e diretorios 
-ls -lh = 
+ls -lh = listar todos os diretorios 
 ctrl + l = limpar
 cd + / = andar entre diretorios 
 cd = entrar nos diretorios
