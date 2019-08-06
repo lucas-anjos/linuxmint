03cf6f5d07df6b28af258e93321116f0cdc13ecf
lucas-anjos <53832530+lucas-anjos@users.noreply.github.com>
2019-08-06 15:19:48 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,16 @@
 Lucas dos Anjos Moreira, senac tatuape, aprendendo git e github...
+~= diretorio pessoal
+ctrl + alt + t = terminal no ambiente grafico
+ctrl + alt + f1 = terminal sem ambiente grafico
+ls = listar arquivos e diretorios 
+ls -lh = 
+ctrl + l = limpar
+cd + / = andar entre diretorios 
+cd = entrar nos diretorios
+azul escuro = diretorio 
+azul claro = link simbolico/atalho
+cinza = arquivos
+marca texto verda = perigo 
+cd - = voltar 
+pwd = verificar o diretorio 
+mkdir = criar diretorio
